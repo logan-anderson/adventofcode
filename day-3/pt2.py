b2673c46549318863f10e9fe1720c2f26593d7d2
Logan Anderson <landerson2@upei.ca>
2022-12-03 11:26:05 -0500

    did day 3

---
+++ day-3/pt2.py
@@ -0,0 +1,80 @@
+
+# Dont love this but code pilot type it out for me
+p = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+}
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def get_common(a, b):
+    return [i for i in a if i in b]
+
+
+inpt = open('input.txt').read()
+lines = inpt.split('\n')
+
+common = []
+
+for chuck in chunks(lines, 3):
+    c1 = get_common(chuck[0], chuck[1])
+    c2 = get_common(chuck[2], c1)
+    common.append(c2[0])
+
+
+total = map(lambda x: p[x], common)
+print(sum(total))
