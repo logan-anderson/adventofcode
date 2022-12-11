083b0086abf904c9117f18a76fa0fa352f9399ac
Logan Anderson <landerson2@upei.ca>
2022-12-11 12:52:24 -0500

    added day 10

---
+++ day-10/p2.py
@@ -0,0 +1,178 @@
+inpt = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+inpt = open('input.txt').read()
+
+cycles = []
+
+for line in inpt.splitlines():
+    cmds = line.split(" ")
+    if(cmds[0] == 'noop'):
+        cycles.append(0)
+    else:
+        cycles.append(0)
+        cycles.append(int(cmds[1].strip()))
+
+
+pos = 1
+pixels = []
+for i, c in enumerate(cycles):
+    pixelPos = (i % 40)
+    # Are we in the range
+    if pos-1 <= pixelPos and pixelPos <= pos + 1:
+        pixels.append('x')
+    else:
+        pixels.append(' ')
+
+    pos = pos + c
+
+for i in range(len(pixels)):
+    if (i) % 40 == 0:
+        print()
+    print(pixels[i], end="")
+
+print()
